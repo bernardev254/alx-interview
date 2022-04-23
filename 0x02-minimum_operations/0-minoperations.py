d6f47f426ea6d998919c8be3952d1269449a6322
bkaraba <bkaraba14@gmail.com>
2022-04-23 08:44:21 +0000

    updates

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" module containing a func to determine min ops"""
+
+
+def minOperations(n: int) -> int:
+    written = 1
+    steps = 1
+    clipboard = 1
+
+    if n <= 0:
+        return 0
+
+    steps += 1
+    written += clipboard
+    rem = n - written
+    to_copy = written
+
+    while rem > 0:
+        if rem % to_copy == 0:
+            if (rem - to_copy) % (to_copy + clipboard) == 0:
+                # paste
+                steps += 1
+                written += clipboard
+                rem = n - written
+            else:
+                # copy
+                steps += 1
+                clipboard = 0
+                clipboard = to_copy = written
+                # paste
+                steps += 1
+                written += clipboard
+                rem = n - written
+        else:
+            # paste
+            steps += 1
+            written += clipboard
+            rem = n - written
+    return steps
