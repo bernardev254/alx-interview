d577d9ed52886e0871278a8f2bdc0647ec10ad51
bkaraba <bkaraba14@gmail.com>
2022-04-30 07:38:01 +0000

    updates

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""script tha reads stdin and computes metrics"""
+
+
+import sys
+from collections import OrderedDict
+
+
+def my_split(line):
+    """my split func"""
+    my_list = []
+    for word in line.strip().split(" "):
+        my_list.append(word)
+
+    return my_list
+
+
+def status_tally(status, line_list):
+    """func returning status tally"""
+    try:
+        status[line_list] += 1
+    except KeyError:
+        pass
+
+
+def print_status(status):
+    """func printing status code  frequency"""
+    for k, v in OrderedDict(status).items():
+        if v is not 0:
+            print("{}: {}".format(k, v))
+
+if __name__ == "__main__":
+    status = {"200": 0, "301": 0, "400": 0,
+              "401": 0, "403": 0, "404": 0,
+              "405": 0, "500": 0}
+
+    times = 0
+    size = 0
+
+    try:
+        for lines in sys.stdin:
+            size += int(my_split(lines)[8])
+
+            if times is not 0 and times % 9 == 0:
+                print("File size: {}".format(size))
+                status_tally(status, my_split(lines)[7])
+                print_status(status)
+            times += 1
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        print("File size: {}".format(size))
+        print_status(status)
+        sys.exit(0)
