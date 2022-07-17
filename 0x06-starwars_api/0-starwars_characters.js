36035e71e78dc1b1e1d6bd51c99f230d4bd1f610
Bkaraba <bkaraba14@gmail.com>
2022-07-17 08:59:02 +0000

    fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,14 +5,14 @@ const request = require('request');
 let characters = [];
 const movieId = parseInt(process.argv[2]);
 
-request(`https://swapi-api.hbtn.io/api/films/${movieId}/`, (error, response, body) => {
+request(`https://swapi-api.hbtn.io/api/films/${movieId}/`, async (error, response, body) => {
   if (!error && response) {
-    characters = JSON.parse(body).characters;
+    characters = await  JSON.parse(body).characters;
 
-    for (let idx = 0; idx < characters.length; idx++) {
-      request(characters[idx], (error, response, body) => {
+    for (let character of characters) {
+      request(character, async (error, response, body) => {
         if (!error && response) {
-          console.log(JSON.parse(body).name);
+          console.log(async JSON.parse(body).name);
         }
       });
     }
