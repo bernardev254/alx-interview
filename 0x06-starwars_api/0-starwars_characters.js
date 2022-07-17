4220d44ddff31fad2ad522e927dfed17178e45ec
Bkaraba <bkaraba14@gmail.com>
2022-07-17 09:25:33 +0000

    updates

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,13 +7,16 @@ const movieId = parseInt(process.argv[2]);
 
 request(`https://swapi-api.hbtn.io/api/films/${movieId}/`, async (error, response, body) => {
   if (!error && response) {
-    characters = await  JSON.parse(body).characters;
+    characters = await JSON.parse(body).characters;
 
-    for (let character of characters) {
-      request(character, async (error, response, body) => {
-        if (!error && response) {
-          console.log(await JSON.parse(body).name);
-        }
+    for (const character of characters) {
+      await new Promise(function (resolve, reject) {
+        request(character, (error, response, body) => {
+          if (!error && response) {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
+        });
       });
     }
   }
